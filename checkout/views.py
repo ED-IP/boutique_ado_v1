09691d5775a86f207d81aeed62dea73b2d65f6a8
ED-IP <edip_courses@outlook.es>
2021-07-19 10:59:59 +0000

    added stripe element

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JEtQNK3YBNnqdcTF6ZOOwdCIc0IzxpVWrfVqKcDoRHcoMXVcaDCvAoPt82jKC3Q4pQQ0bgtQELpXh3E9PoLYKfe00c5TWVwJt',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
